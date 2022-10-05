2a5ac953d8bb68aac4dffeee0fd1f6a7c9f9cf39
Jonathan Dev <jonathan.aldana170500@gmail.com>
2022-10-05 12:23:11 -0500

    Port order

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import { config } from "dotenv";
 config();
 export const DB_PORT = 3306;
-export const PORT = 6000;
+export const PORT = process.env.PORT;
 export const DB_HOST = process.env.DB_HOST ||'localhost';
 export const DB_USER = process.env.DB_USER || 'root';
 export const DB_PASSWORD = process.env.DB_PASSWORD || '12345678';
