9d5ce78208d6aafc984d85dac9113203bcc0afb6
Jonathan Dev <jonathan.aldana170500@gmail.com>
2022-10-05 04:26:58 -0500

    CONFIG

--- src/db.js
+++ src/db.js
@@ -1,5 +1,5 @@
 import { createPool } from "mysql2/promise";
-import { DB_HOST, DB_DATABASE, DB_USER, DB_PASSWORD, PORT, DB_PORT } from "../config.js";
+import { DB_HOST, DB_DATABASE, DB_USER, DB_PASSWORD, PORT, DB_PORT } from "./config.js";
 export const pool = createPool({
     host:DB_HOST,
     user:DB_USER,
