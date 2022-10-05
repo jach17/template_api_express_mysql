69a122581cdd06a5e0b3196ad85f72203b56c836
Jonathan Dev <jonathan.aldana170500@gmail.com>
2022-10-05 14:03:57 -0500

    Ortografia!

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import { config } from "dotenv";
 config();
-export const DB_PORT = 3306;
+export const DB_PORT = process.env.DB_PORT;
 export const PORT = process.env.PORT;
 export const DB_HOST = process.env.DB_HOST ||'localhost';
 export const DB_USER = process.env.DB_USER || 'root';
