224069875d781cdfb9b5328055db5efdc414826b
Jonathan Dev <jonathan.aldana170500@gmail.com>
2022-10-05 04:09:31 -0500

    POrts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import app from "./app.js";
-import {DB_PORT} from "../config.js"
+import {PORT} from "../config.js"
 
-app.listen(DB_PORT, () => {
-    console.log(`Server started on port ${DB_PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 });
