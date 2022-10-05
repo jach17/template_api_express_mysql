b0dfa624f8921686ea4352e325a61a062506291e
Jonathan Dev <jonathan.aldana170500@gmail.com>
2022-10-05 13:17:34 -0500

    DB name

--- src/config.js
+++ src/config.js
@@ -5,4 +5,4 @@ export const PORT = process.env.PORT;
 export const DB_HOST = process.env.DB_HOST ||'localhost';
 export const DB_USER = process.env.DB_USER || 'root';
 export const DB_PASSWORD = process.env.DB_PASSWORD || '12345678';
-export const DB_DATABASE = process.env.DB_DATABASE = 'compannydb';
+export const DB_DATABASE = process.env.DB_DATABASE || 'compannydb';
